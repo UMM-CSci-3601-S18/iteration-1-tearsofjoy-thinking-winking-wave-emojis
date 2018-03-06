e1bc4660e012d8e1d4bd5facf84192f3b0daa436
miyat001 <miyat001@morris.umn.edu>
2018-03-06 15:10:43 -0600

    Working on response page function that expand panel

--- client/src/app/response/response.component.ts
+++ client/src/app/response/response.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {MatExpansionModule} from '@angular/material/expansion'
 
 @Component({
     selector: 'app-response-component',
@@ -8,10 +9,34 @@ import {Component, OnInit} from '@angular/core';
 
 export class ResponseComponent implements OnInit {
 
+    step = -1;
+
+    setStep(index: number) {
+        if(window.location.href.indexOf ("http://localhost:9000/response#happyMessage")){
+            this.step = index;
+
+        }
+    }
+
+    nextStep() {
+        this.step++;
+    }
+
+    prevStep() {
+        this.step--;
+    }
+
     loadService(): void {
     }
 
     ngOnInit(): void {
         this.loadService();
     }
+
+
+    testopenexpansion(): void{
+
+    }
+
+
 }
